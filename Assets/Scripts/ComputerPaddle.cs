7936d7fa12c7469721f606109dc6440a51e2ca93
Adam Graham <adam@zigurous.com>
2022-01-27 20:31:50 -0600

    Change code formatting style

--- Assets/Scripts/ComputerPaddle.cs
+++ Assets/Scripts/ComputerPaddle.cs
@@ -8,23 +8,23 @@ public class ComputerPaddle : Paddle
     {
         // Check if the ball is moving towards the paddle (positive x velocity)
         // or away from the paddle (negative x velocity)
-        if (this.ball.velocity.x > 0f)
+        if (ball.velocity.x > 0f)
         {
             // Move the paddle in the direction of the ball to track it
-            if (this.ball.position.y > this.rigidbody.position.y) {
-                this.rigidbody.AddForce(Vector2.up * this.speed);
-            } else if (this.ball.position.y < this.rigidbody.position.y) {
-                this.rigidbody.AddForce(Vector2.down * this.speed);
+            if (ball.position.y > rigidbody.position.y) {
+                rigidbody.AddForce(Vector2.up * speed);
+            } else if (ball.position.y < rigidbody.position.y) {
+                rigidbody.AddForce(Vector2.down * speed);
             }
         }
         else
         {
             // Move towards the center of the field and idle there until the
             // ball starts coming towards the paddle again
-            if (this.rigidbody.position.y > 0f) {
-                this.rigidbody.AddForce(Vector2.down * this.speed);
-            } else if (this.rigidbody.position.y < 0f) {
-                this.rigidbody.AddForce(Vector2.up * this.speed);
+            if (rigidbody.position.y > 0f) {
+                rigidbody.AddForce(Vector2.down * speed);
+            } else if (rigidbody.position.y < 0f) {
+                rigidbody.AddForce(Vector2.up * speed);
             }
         }
     }
--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -7,18 +7,18 @@ public class PlayerPaddle : Paddle
     private void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            this.direction = Vector2.up;
+            direction = Vector2.up;
         } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            this.direction = Vector2.down;
+            direction = Vector2.down;
         } else {
-            this.direction = Vector2.zero;
+            direction = Vector2.zero;
         }
     }
 
     private void FixedUpdate()
     {
-        if (this.direction.sqrMagnitude != 0) {
-            this.rigidbody.AddForce(this.direction * this.speed);
+        if (direction.sqrMagnitude != 0) {
+            rigidbody.AddForce(direction * speed);
         }
     }
 
