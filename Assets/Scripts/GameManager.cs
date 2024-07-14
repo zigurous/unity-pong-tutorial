5708339edd4a8e5f630e9f11dfe5ee1ce9db32f9
Adam Graham <adam@zigurous.com>
2024-07-13 19:36:50 -0500

    Simplify documentation comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,49 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// Manages the state of the game such as scoring and starting new rounds.
-/// </summary>
 public class GameManager : MonoBehaviour
 {
-    /// <summary>
-    /// The ball component.
-    /// </summary>
-    [Tooltip("The ball component.")]
     public Ball ball;
 
-    /// <summary>
-    /// The player's paddle component.
-    /// </summary>
-    [Tooltip("The player's paddle component.")]
     public Paddle playerPaddle;
-
-    /// <summary>
-    /// The current score of the player.
-    /// </summary>
     public int playerScore { get; private set; }
-
-    /// <summary>
-    /// The UI text that displays the player's score.
-    /// </summary>
-    [Tooltip("The UI text the displays the player's score.")]
     public Text playerScoreText;
 
-    /// <summary>
-    /// The computer's paddle component.
-    /// </summary>
-    [Tooltip("The computer's paddle component.")]
     public Paddle computerPaddle;
-
-    /// <summary>
-    /// The current score of the computer.
-    /// </summary>
     public int computerScore { get; private set; }
-
-    /// <summary>
-    /// The UI text that displays the computer's score.
-    /// </summary>
-    [Tooltip("The UI text the displays the computer's score.")]
     public Text computerScoreText;
 
     private void Start()
--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -1,18 +1,11 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Handles moving the player's paddle based on user input.
-/// </summary>
 public class PlayerPaddle : Paddle
 {
-    /// <summary>
-    /// The direction the player is moving.
-    /// </summary>
     public Vector2 direction { get; private set; }
 
     private void Update()
     {
-        // Set the direction of the paddle based on the input key being pressed
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
             this.direction = Vector2.up;
         } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
@@ -24,7 +17,6 @@ public class PlayerPaddle : Paddle
 
     private void FixedUpdate()
     {
-        // Move the paddle by applying a force in the set direction
         if (this.direction.sqrMagnitude != 0) {
             this.rigidbody.AddForce(this.direction * this.speed);
         }
